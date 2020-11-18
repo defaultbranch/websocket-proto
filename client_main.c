e7c4b6f8a258b4ddb1d299318e6c86afd59e4ef1
Christian Fuchs <cfuchs@scs.ch>
2020-11-18 07:53:17 +0100

    starting point: minimal main routines

--- client_main.c
+++ client_main.c
@@ -5,13 +5,6 @@
 #include <libwebsockets.h>
 
 
-#define LWS_PLUGIN_STATIC
-#if !defined (LWS_PLUGIN_STATIC)
-#define LWS_DLL
-#define LWS_INTERNAL
-#include <libwebsockets.h>
-#endif
-
 int main(int argc, char **argv) {
     printf("Client started\n");
     return 0;
